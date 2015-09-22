6c913dda90419afc6439aca3606c301fd38b437d
858385135@qq.com <858385135@qq.com>
2015-09-22 20:32:32 +0800

    视图完成首页和发表

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,60 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/index', function(req, res, next) {
+
+  var locals={
+    title:'木鱼网络',
+    section:'index',
+    navlinks:{
+      index:{
+        label:'index',
+        key:'内容管理',
+        icon:'glyphicon-home'
+      },
+      post:{
+        label:'post',
+        key:'内容增加',
+        icon:'glyphicon-time'
+
+      },
+      comment:{
+        label:'comment',
+        key:'回复管理',
+        icon:'glyphicon-user'
+      }
+    }
+  }
+
+  res.render('index', locals);
+});
+
+router.get("/post", function(req, res, next) {
+
+  var locals={
+    title:'木鱼网络',
+    section:'index',
+    navlinks:{
+      index:{
+        label:'index',
+        key:'内容管理',
+        icon:'glyphicon-home'
+      },
+      post:{
+        label:'post',
+        key:'内容增加',
+        icon:'glyphicon-time'
+
+      },
+      comment:{
+        label:'comment',
+        key:'回复管理',
+        icon:'glyphicon-user'
+      }
+    }
+  }
+
+  res.render('post', locals);
 });
 
 module.exports = router;
