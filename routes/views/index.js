3b316eda04d6c2c5f1131f9966591d9e15bcfa94
whw <wanghaowen_1988@163.com>
2015-09-26 15:22:16 +0800

    翻页功能完成， 下一步： 指定页跳转，条件查询

--- routes/views/index.js
+++ routes/views/index.js
@@ -3,6 +3,7 @@
  */
 var url = require('url');
 
+
 var debug = require('debug')('app:index');
 var findArgs = {
   searchField : 'data.title date',
@@ -31,20 +32,18 @@ exports = module.exports = function  (req, res) {
     locals.pages.tol = count / findArgs.itemPerPage + (0 == count % findArgs.itemPerPage ? 0 : 1);
 
     // revise the page number, and it is also the current page number
-    var urlParse = url.parse(req.url).query;
-    var pNum = urlParse.p;
-    if (typeof pNum != 'number' || pNum <= 0) {
+    var urlQuery = url.parse(req.url, true).query;
+    var pNum = parseInt(urlQuery.p, 10);
+    if (isNaN(pNum) || pNum <= 0) {
       pNum = 1;
     }
+
     if (pNum > locals.pages.tol) {
       pNum = locals.pages.tol;
     }
     locals.pages.cur = pNum;
 
-    modelBlog.find({}, findArgs.searchField, getData)
-      .sort(findArgs.sortArgs)
-      .skip((pNum-1) * findArgs.itemPerPage)
-      .limit(findArgs.itemPerPage);
+    modelBlog.find({}, findArgs.searchField, getData).sort(findArgs.sortArgs).skip((pNum-1) * findArgs.itemPerPage) .limit(findArgs.itemPerPage);
   }
 
   function getData(err, data){
@@ -54,8 +53,9 @@ exports = module.exports = function  (req, res) {
       return;
     }
 
+    var index = 1 + (locals.pages.cur - 1) * findArgs.itemPerPage;
     for (i in data) {
-      locals.blogs.push({index: findArgs.skip++, title: data[i].data.title, date: data[i].date});
+      locals.blogs.push({index: index++, title: data[i].data.title, date: data[i].date});
     }
 
     res.render('index', locals);
